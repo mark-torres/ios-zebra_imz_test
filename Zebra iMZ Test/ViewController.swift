50eb6d56eba35fc6dcceebfcd6b4e72394c8cd20
Mark Torres <mark.torres.mets@gmail.com>
2017-06-27 12:38:32 -0500

    Layout

--- Zebra iMZ Test/ViewController.swift
+++ Zebra iMZ Test/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet weak var textMessage: UITextField!
+	
+	@IBOutlet weak var imageView: UIImageView!
+	
+	@IBOutlet weak var loaderView: UIView!
+	
+	@IBOutlet weak var loaderSpinner: UIActivityIndicatorView!
+	
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -20,6 +29,35 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	// MARK - IB Actions
+	
+	@IBAction func tapPrintText(_ sender: AnyObject) {
+		showAlert(asError: false, withMessage: "Tapped 'Print text'")
+	}
+	
+	@IBAction func tapChooseImage(_ sender: AnyObject) {
+		showAlert(asError: false, withMessage: "Tapped 'Choose image'")
+	}
+	
+	@IBAction func tapPrintImage(_ sender: AnyObject) {
+		showAlert(asError: false, withMessage: "Tapped 'Print image'")
+	}
+	
+	// MARK - iMZ printer
+	
+	func printTextCpcl(text: String) -> Void {
+	}
+	
+	func printImageCpcl() -> Void {
+	}
+	
+	// MARK - Misc methods
+	
+	func showAlert(asError isError: Bool, withMessage message: String) -> Void {
+		let alert = UIAlertController(title: isError ? "Error" : "Info", message: message, preferredStyle: .alert)
+		let actionOk = UIAlertAction(title: "OK", style: .default, handler: nil)
+		alert.addAction(actionOk)
+		present(alert, animated: true, completion: nil)
+	}
 }
 
---
+++ Zebra iMZ Test/Zebra iMZ Test-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "MFiBtPrinterConnection.h"
+#import "ZebraPrinter.h"
+#import "ZebraPrinterFactory.h"
