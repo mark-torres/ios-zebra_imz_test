baaa1d375b823f1165c54c80c5fbe5b1f320b591
Mark Torres <mark.torres.mets@gmail.com>
2017-06-27 13:09:13 -0500

    Print text

--- Zebra iMZ Test/ViewController.swift
+++ Zebra iMZ Test/ViewController.swift
@@ -32,7 +32,10 @@ class ViewController: UIViewController {
 	// MARK - IB Actions
 	
 	@IBAction func tapPrintText(_ sender: AnyObject) {
-		showAlert(asError: false, withMessage: "Tapped 'Print text'")
+		let text = textMessage.text?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) ?? ""
+		if !text.isEmpty {
+			printTextCpcl(text)
+		}
 	}
 	
 	@IBAction func tapChooseImage(_ sender: AnyObject) {
@@ -45,13 +48,81 @@ class ViewController: UIViewController {
 	
 	// MARK - iMZ printer
 	
-	func printTextCpcl(text: String) -> Void {
+	func getFirstBtPrinter() -> String {
+		var serialNumber: String = ""
+		
+		// Find the Zebra BT Accessory
+		let sam: EAAccessoryManager = EAAccessoryManager.shared()
+		let connectedAccessories = sam.connectedAccessories
+		for accessory in connectedAccessories {
+			// Note: This will find the first printer connected!
+			// If you have multiple Zebra printers connected,
+			// you should display a list to the user and have
+			// him select the one they wish to use
+			if accessory.protocolStrings.index(of: "com.zebra.rawport")! >= 0 {
+				serialNumber = accessory.serialNumber
+				break
+			}
+		}
+		
+		return serialNumber
+	}
+	
+	func printTextCpcl(_ text: String) -> Void {
+		showLoader()
+		DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
+			var errorMsg = ""
+			var nsError: NSError? = nil
+			
+			let serialNumber = self.getFirstBtPrinter()
+			
+			if serialNumber.isEmpty {
+				errorMsg = "No BT printer found"
+			} else {
+				// Instantiate connection to Zebra Bluetooth accessory
+				let thePrinterConn = MfiBtPrinterConnection.init(serialNumber: serialNumber)
+				
+				// Open the connection - physical connection is established here.
+				if !(thePrinterConn?.open() ?? false) {
+					errorMsg = "Error opening connection to printer " + serialNumber
+				} else {
+					let cpclString = "! 0 200 200 110 1\r\nTEXT 4 0 5 5 \(text)\r\nFORM\r\nPRINT\r\n"
+					
+					let writtenBytes = thePrinterConn?.write(cpclString.data(using: String.Encoding.utf8), error: &nsError) ?? -1
+					
+					if writtenBytes < 0 || nsError != nil {
+						errorMsg = "Error writing to the printer " + serialNumber
+					}
+					
+					// close printer connection
+					thePrinterConn?.close()
+				}
+			}
+			
+			DispatchQueue.main.async {
+				self.hideLoader()
+				
+				if !errorMsg.isEmpty {
+					self.showAlert(asError: true, withMessage: errorMsg)
+				}
+			}
+		}
 	}
 	
 	func printImageCpcl() -> Void {
 	}
 	
 	// MARK - Misc methods
+	
+	func showLoader() {
+		loaderView.isHidden = false
+		loaderSpinner.startAnimating()
+	}
+	
+	func hideLoader() {
+		loaderView.isHidden = true
+		loaderSpinner.stopAnimating()
+	}
 	
 	func showAlert(asError isError: Bool, withMessage message: String) -> Void {
 		let alert = UIAlertController(title: isError ? "Error" : "Info", message: message, preferredStyle: .alert)
